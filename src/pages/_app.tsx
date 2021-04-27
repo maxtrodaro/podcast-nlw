f47507873fa6147808aca5565567ce6a1b9946c8
Eduardo Godoy <dudurossigodoy2010@gmail.com>
2021-04-27 02:29:16 -0300

    Header, Player component and Generate SSG

---
+++ src/pages/_app.tsx
@@ -0,0 +1,20 @@
+import "../styles/global.scss";
+
+import Header from "../components/Header/Header";
+import Player from "../components/Player/Player";
+
+import styles from "../styles/app.module.scss";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <div className={styles.wrapper}>
+      <main>
+        <Header />
+        <Component {...pageProps} />
+      </main>
+      <Player />
+    </div>
+  );
+}
+
+export default MyApp;
